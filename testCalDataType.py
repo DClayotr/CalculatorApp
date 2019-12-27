b2333a7a8e258c46c68b39e6c0efb6bd8f898897
Dyaln K Claytor <32943321+DClayotr@users.noreply.github.com>
2019-12-26 23:34:25 -0500

    Add files via upload
    unit test for CalDataType
    tests:
    -all 4 operators return proper values

---
+++ testCalDataType.py
@@ -0,0 +1,20 @@
+#unit test for calDataType.py
+
+import unittest
+from CalDataType import calculate
+
+class TestCalculate(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(calculate('+',3,4), 7)
+
+    def test_sub(self):
+        self.assertEqual(calculate('-',4,3), 1)
+
+    def test_mult(self):
+        self.assertEqual(calculate('*',3,4),12)
+
+    def test_div(self):
+        self.assertAlmostEqual(calculate('/',4,5),0.8)
+
+
