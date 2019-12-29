2660eef4d3efc85f041baf402ae39e43c3508abf
Dyaln K Claytor <32943321+DClayotr@users.noreply.github.com>
2019-12-29 16:59:57 -0500

    Update testEqParse.py
    Added test for exponent operator

--- testEqParse.py
+++ testEqParse.py
@@ -5,5 +5,5 @@ from EqParse import EqParse
 class TestEquationParse(unittest.TestCase):
 
     def test_EQ(self):
-        eqTestArr = [716565.583,'/','(',82156,'*',6554,'-',2222,')']
-        self.assertEqual(EqParse("716565.583/.//(82156*6554-2222)"), eqTestArr) 
+        eqTestArr = [716565.583,'^','(',2,')','/','(',82156,'*',6554,'-',2222,')']
+        self.assertEqual(EqParse("716565.583^(2)/.//(82156*6554-2222)"), eqTestArr) 
