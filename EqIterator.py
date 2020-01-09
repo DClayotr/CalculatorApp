00688b968a32c2fc876195c346b3404bed32d475
Dyaln K Claytor <32943321+DClayotr@users.noreply.github.com>
2020-01-09 13:19:40 -0500

    Update EqIterator.py
    Adds '()' functionality

--- EqIterator.py
+++ EqIterator.py
@@ -4,40 +4,49 @@
 from CalDataType import calculate 
 from EqParse import EqParse
 
-def eqIterator(eqArray_temp):
+def eqIterator(eqArray):
+
+    
 
     calculation = False
     index = 0
-
-    eqArray = []
-    eqArray = EqParse(eqArray_temp)
-
+ 
     leftNum = 0
     rightNum = 0
 
-    PEMDAS = ['^', '*', '/', '+', '-']
+    PEMDAS = ['(','^', '*', '/', '+', '-']
 
     while index < len(PEMDAS):
  
         
-        for i in range(len(eqArray)):
+        for i in range(len(eqArray)-1):
             
             place = eqArray[i]
-            
-            if(place == PEMDAS[index]):
-                
-                leftNum = eqArray[i-1]
-                rightNum = eqArray[i+1]
-                eqArray[i] = calculate(place, leftNum, rightNum)
+            print(place)
 
-                eqArray.pop(i-1)
-                eqArray.pop(i)
-
-                calculation = True
+            if (place == '('):
+                last_closing = len(eqArray) - 1 - eqArray[::-1].index(')')
+                recur_list = eqArray[i+1:last_closing]
+                eqArray[i] = eqIterator(recur_list)
+                del eqArray[i+1:last_closing+1]
                 break
-                
+
+
             else:
-                continue
+                if(place == PEMDAS[index]):
+                
+                    leftNum = eqArray[i-1]
+                    rightNum = eqArray[i+1]
+                    eqArray[i] = calculate(place, leftNum, rightNum)
+
+                    eqArray.pop(i-1)
+                    eqArray.pop(i)
+
+                    calculation = True
+                    break
+                
+                else:
+                    continue
 
         if(calculation == True):
             calculation = False
@@ -48,9 +57,9 @@ def eqIterator(eqArray_temp):
         
 
 
-    return eqArray[0]         
+    return eqArray[0]        
                 
         
 
 x = str(input("Please enter an expression to be evaluated.\n> "))
-print(eqIterator(x))
+print(eqIterator(EqParse(x.strip())))
